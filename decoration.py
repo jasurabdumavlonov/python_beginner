727b417fd22fbd001599a580e7d4045383d8b2ca
JR <jasurackerman2004@gmail.com>
2024-04-18 18:40:15 +0500

    break statement beginner

---
+++ decoration.py
@@ -0,0 +1,23 @@
+# // 1st function
+# def inner_function():
+#     print("inner function")
+#
+# def outer_function(func):
+#     print("outer function")
+#     func()
+#
+# outer_function(inner_function)
+
+#   // 2nd function
+# def outter_function():
+#     print("outter function")
+#
+#     def inner_function():
+#         print("inner function")
+#
+#     return inner_function
+#
+# returned = outter_function()
+#
+# returned()
+
---
+++ file_handling.py
@@ -0,0 +1,33 @@
+# file = open("geeks.txt", "r")
+
+# print(file.read())
+
+# for i in file:
+#     print(i)
+
+# with open("geeks.txt") as file:
+#     data = file.read()
+#     print(data)
+
+# file = open("geeks.txt", "r")
+# print(file.read(5))
+
+# with open("geeks.txt", "r") as file:
+#     data = file.readlines()
+#     for line in data:
+#         word = line.split()
+#         print(word)
+
+# file = open("geeks.txt", "w")
+# file.write("this is the written programm")
+# file.write("second written")
+# file.close()
+
+# with open("geeks.txt", "w") as file:
+#     file.write("hello, world")
+
+# file = open("geeks.txt", "a")
+# file.write("hello, file")
+# file.write("hello, programm")
+# file.close()
+
