d8cb1eb8dffd5844cc2977317f9eaf76a1a5b24d
JR <jasurackerman2004@gmail.com>
2024-04-18 17:39:01 +0500

    break statement beginner

---
+++ hello.py
@@ -0,0 +1,14 @@
+usernames = ["Jade", "John", "bob", "David"]
+
+# i = 0
+#
+# while i < len(usernames):
+#     print(usernames[i])
+#     if usernames[i] == "John":
+#         break
+#     i += 1
+
+# for i in usernames:
+#     print(i)
+#     if i == "John":
+#         break
